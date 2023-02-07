3d7653f65308c786559ba7f18d3af0e6497c1778
MLD4 <dongmiao123@gmail.com>
2023-02-07 22:31:27 +1030

    formating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,30 @@
-var weatherInput = document.querySelector('#weatherForm');
+const weatherInput = document.querySelector('#weatherForm');
+const cityDislayArea = document.querySelector('#pastCitySearches');
+const citySelect = document.querySelectorAll('#citySelect');
 var cityList = [];
-var cityDislayArea = document.querySelector('#pastCitySearches');
-var citySelect = document.querySelectorAll('#citySelect')
 
-weatherInput.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let userInput = document.querySelector('#cityUserInput').value;
-    getWeatherData(userInput);
-    checkCity(userInput);
-    document.querySelector('#forecast').innerHTML = '';
-    clearDisplayCities();
-    displayCities();
-})
-
-function getWeatherData(userInput) {
-    
-    var API = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=b7a55a602c54c17bc010a65a5bf43262&units=metric`
+function getWeatherData(userInput) { //call api data
+    let API = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=b7a55a602c54c17bc010a65a5bf43262&units=metric`
     fetch(API)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             document.querySelector('#cityName').innerHTML = userInput;
-            var allWeatherData = data.list.slice(0, 40);
+            let allWeatherData = data.list.slice(0, 40); //get bit with weather data
             console.log(allWeatherData);
 
             for (i = 0; i < allWeatherData.length; i++) {
-                if (allWeatherData[i].dt_txt.search("15:00:00") != -1) {
-                    var timeStamp = new Date(allWeatherData[i].dt * 1000);
-                    var day = timeStamp.toLocaleString("en-GB", { weekday: "long" })
-                    var date = timeStamp.toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "2-digit" })
-                    var icon = allWeatherData[i].weather[0].icon;
-                    var temp = allWeatherData[i].main.temp;
-                    var wind = allWeatherData[i].wind.speed;
-                    var hum = allWeatherData[i].main.humidity;
+                if (allWeatherData[i].dt_txt.search("15:00:00") != -1) { // check to see if 3pm 
+                    let timeStamp = new Date(allWeatherData[i].dt * 1000); //conversion of unix time to ms
+                    let day = timeStamp.toLocaleString("en-GB", { weekday: "long" })
+                    let date = timeStamp.toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "2-digit" })
+                    let icon = allWeatherData[i].weather[0].icon;
+                    let temp = allWeatherData[i].main.temp;
+                    let wind = allWeatherData[i].wind.speed;
+                    let hum = allWeatherData[i].main.humidity;
                     console.log(allWeatherData[i].dt_txt);
-
-                    var weatherDayDisplay = document.createElement('div');
+                    let weatherDayDisplay = document.createElement('div');
                     weatherDayDisplay.classList.add('col')
                     weatherDayDisplay.innerHTML = `
                         <div>${day}</div>
@@ -48,16 +36,15 @@ function getWeatherData(userInput) {
                     document.querySelector('#forecast').appendChild(weatherDayDisplay);
                 }
             }
-        }
-    )
+        })
 }
 
-function checkCity(userInput) {
-    if(localStorage.getItem("cityHistory") === null){
+function checkCity(userInput) { //all the ifs and else are just to check if the same data is already in local storage
+    if (localStorage.getItem("cityHistory") === null) { // the has to be a better way for for the first check. on start check local storage?
         console.log('empty')
         cityList.push(userInput);
         localStorage.setItem('cityHistory', JSON.stringify(cityList));
-    }else{
+    } else {
         let obj = localStorage.getItem('cityHistory');
         let cityHistoryList = JSON.parse(obj);
         if (cityHistoryList.includes(userInput) == false) {
@@ -70,10 +57,10 @@ function checkCity(userInput) {
     };
 }
 
-function displayCities() {
+function displayCities() { //displays local storage data
     let obj = localStorage.getItem('cityHistory');
     let cityHistoryList = JSON.parse(obj);
-    for (i = 0; i < cityHistoryList.length; i++){
+    for (i = 0; i < cityHistoryList.length; i++) {
         let city = document.createElement('div');
         city.setAttribute("onclick", "searchSaveCity(event)");
         city.setAttribute("id", `${cityHistoryList[i]}`)
@@ -82,7 +69,7 @@ function displayCities() {
     }
 }
 
-function clearDisplayCities(){
+function clearDisplayCities() { 
     cityDislayArea.innerHTML = '';
 }
 
@@ -92,4 +79,16 @@ function searchSaveCity(event) {
     let userInput = text;
     getWeatherData(userInput)
 }
+
+weatherInput.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let userInput = document.querySelector('#cityUserInput').value;
+    getWeatherData(userInput);
+    checkCity(userInput);
+    document.querySelector('#forecast').innerHTML = '';
+    clearDisplayCities();
+    displayCities();
+})
+
 displayCities()
+
